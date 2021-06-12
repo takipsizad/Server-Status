15ac9c3539244c001b5f1a659151e93a5a591c54
Aalman Mohammed <70076286+AwareSuperCC@users.noreply.github.com>
2021-06-12 16:11:50 +0400

    Setup File

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+pip3 install discord.py
+pip3 install mcstatus
+pip3 install pyhton-decouple
+echo "Enter server Address"
+read address
+echo "ADDRESS=${address}" > .env
+echo "Enter Discord Bot TOKEN"
+read TOKEN
+echo "TOKEN=${TOKEN}" >> .env
+echo "DONE!"
