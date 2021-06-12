e33ac67644370c8c8afe4bd10cf45e262ddf4374
Aalman Mohammed <70076286+AwareSuperCC@users.noreply.github.com>
2021-06-12 16:21:44 +0400

    Setup File

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 pip3 install discord.py
 pip3 install mcstatus
-pip3 install pyhton-decouple
+pip3 install python-decouple
 echo "Enter server Address"
 read address
 echo "ADDRESS=${address}" > .env
